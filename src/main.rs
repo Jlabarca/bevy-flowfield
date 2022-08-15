59c2b9f13d881a4caa5422752c1d181e2a977fae
Julio Labarca <jlabarca25@gmail.com>
2022-08-15 19:33:00 -0400

    Setup 3d scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use bevy::{prelude::*};
+use bevy_flycam::MovementSettings;
+use bevy_flycam::PlayerPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
+        .insert_resource(MovementSettings {
+            sensitivity: 0.00005, // default: 0.00012
+            speed: 32.0,          // default: 12.0
+        })
+        .add_startup_system(setup_scene)
+        .run();
 }
+
+/// set up a simple 3D scene
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+
+    info!("Move camera around by using WASD for lateral movement");
+    info!("Use Left Shift and Spacebar for vertical movement");
+    info!("Use the mouse to look around");
+    info!("Press Esc to hide or show the mouse cursor");
+}
+
